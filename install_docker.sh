a306ecba2bda83a73ce920622387eaabac076a94
n3011 <mrinal.haloi11@gmail.com>
2017-07-18 21:21:33 +0530

    docker install

--- install_docker.sh
+++ install_docker.sh
@@ -6,6 +6,7 @@ sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
+sudo pip install docker-compose
 sudo usermod -a -G docker $USER
 sudo docker run hello-world
 
