ac692a3ec098932b5c881160dcf0c1d7ec943ed1
n3011 <mrinal.haloi11@gmail.com>
2017-07-18 20:50:15 +0530

    sudo problem solved

--- install_docker.sh
+++ install_docker.sh
@@ -6,6 +6,7 @@ sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
+sudo usermod -a -G docker $USER
 sudo docker run hello-world
 
 wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb
